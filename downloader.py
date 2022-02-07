5536eb93a49e0470f71c0f294e6ddfd50f8ef4ac
anuj padhiyar <padhiyaranuj742000@gmail.com>
2022-02-07 18:30:58 +0530

    folder name changed

--- downloader.py
+++ downloader.py
@@ -10,7 +10,7 @@ class YoutubeDownloader:
     def __init__(self):
         self.content = ""
         self.links = []
-        self.location = "YTDownloads/"
+        self.location = "Downloads/"
         self.url = ""
 
         #get the required data from the initiator function
